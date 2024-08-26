130b9738f04784255c801f1fc292ed964c50582a
cagatay.yildiz <cagatay@pixupplay.com>
2024-08-26 09:57:49 +0300

    project init

---
+++ data/products.js
@@ -0,0 +1,28 @@
+//Generate products array. Fields: id, name, price, description
+
+
+let products = [
+    {
+        id: 1,
+        name: 'Apple',
+        price: 0.5,
+        description: 'An apple a day keeps the doctor away'
+    },
+    {
+        id: 2,
+        name: 'Banana',
+        price: 0.3,
+        description: 'Bananas are a great source of potassium'
+    },
+    {
+        id: 3,
+        name: 'Orange',
+        price: 0.7,
+        description: 'Oranges are a great source of vitamin C'
+    }
+]
+
+
+module.exports ={
+    products
+}
